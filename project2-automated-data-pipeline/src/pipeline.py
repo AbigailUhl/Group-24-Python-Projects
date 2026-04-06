cc89244b79339cca5cdac25a9e12d1315c950686
AbigailUhl <abbyuhl2022@gmail.com>
2026-04-06 10:23:14 -0400

    change format to include function and main function

--- project2-automated-data-pipeline/src/pipeline.py
+++ project2-automated-data-pipeline/src/pipeline.py
@@ -1,28 +1,31 @@
 import requests
 import pandas as pd
 from pathlib import Path
-from datetime import datetime
 
 BASE_URL = "https://api.open-meteo.com/v1/forecast"
 
-cities = {
+CITIES = {
     "Tallahassee": {"latitude": 30.4383, "longitude": -84.2807},
     "Miami": {"latitude": 25.7617, "longitude": -80.1918},
     "Atlanta": {"latitude": 33.7490, "longitude": -84.3880}
 }
-all_results = []
 
-for city, coords in cities.items():
+OUTPUT_FILE = Path("data/processed/weather_data.csv")
+OUTPUT_FILE.parent.mkdir(parents=True, exist_ok=True)
+
+def fetch_weather_data(city, coords):
     params = {
-        "latitude": coords["latitude"],
-        "longitude": coords["longitude"],
-        "daily": "temperature_2m_max,temperature_2m_min,precipitation_sum,wind_speed_10m_max",
-        "timezone": "auto"
+            "latitude": coords["latitude"],
+            "longitude": coords["longitude"],
+            "daily": "temperature_2m_max,temperature_2m_min,precipitation_sum,wind_speed_10m_max",
+            "timezone": "auto"
     }
+     
+    results = []
 
     try:
         response = requests.get(BASE_URL, params=params, timeout=10)
-        response.raise_for_status()  # raises error if status is not 200
+        response.raise_for_status() 
 
         data = response.json()
         daily = data["daily"]
@@ -36,7 +39,7 @@ for city, coords in cities.items():
                 "precipitation_sum": daily["precipitation_sum"][i],
                 "wind_speed_max": daily["wind_speed_10m_max"][i]
             }
-            all_results.append(row)
+            results.append(row)
 
     except requests.exceptions.Timeout:
         print(f"Request timed out for {city}")
@@ -44,11 +47,20 @@ for city, coords in cities.items():
     except requests.exceptions.RequestException as e:
         print(f"Request failed for {city}: {e}")
 
-df = pd.DataFrame(all_results)
-OUTPUT_FILE = Path("data/processed/weather_data.csv")
-OUTPUT_FILE.parent.mkdir(parents=True, exist_ok=True)
+    return results
 
-if OUTPUT_FILE.exists():
-    df.to_csv(OUTPUT_FILE, mode="a", header=False, index=False)
-else:
-    df.to_csv(OUTPUT_FILE, index=False)
+def main():
+    all_results = []
+
+    for city, coords in CITIES.items():
+        all_results.extend(fetch_weather_data(city, coords))
+
+    df = pd.DataFrame(all_results)
+
+    if OUTPUT_FILE.exists():
+        df.to_csv(OUTPUT_FILE, mode="a", header=False, index=False)
+    else:
+        df.to_csv(OUTPUT_FILE, index=False)
+
+if __name__ == "__main__":
+    main()
