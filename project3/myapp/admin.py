ef68a3a714814d10d7915d3e643fa73060c9e3b9
AbigailUhl <abbyuhl2022@gmail.com>
2026-04-13 21:13:09 -0400

    Add country team player models and admin setup

--- project3/myapp/admin.py
+++ project3/myapp/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Country, Team, Player
 
-# Register your models here.
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    search_fields = ['name']
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'country']
+    search_fields = ['name']
+    list_filter = ['country']
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'team', 'position', 'overall_rating', 'age']
+    search_fields = ['name', 'nationality']
+    list_filter = ['position', 'team']
--- project3/myapp/models.py
+++ project3/myapp/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Player(models.Model):
+    POSITION_CHOICES = [
+        ('GK', 'Goalkeeper'),
+        ('DEF', 'Defender'),
+        ('MID', 'Midfielder'),
+        ('FWD', 'Forward'),
+    ]
+
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    nationality = models.CharField(max_length=100, blank=True)
+    overall_rating = models.IntegerField()
+    potential = models.IntegerField()
+    pace = models.IntegerField(null=True, blank=True)
+    shooting = models.IntegerField(null=True, blank=True)
+    passing = models.IntegerField(null=True, blank=True)
+    dribbling = models.IntegerField(null=True, blank=True)
+    defending = models.IntegerField(null=True, blank=True)
+    physical = models.IntegerField(null=True, blank=True)
+    position = models.CharField(max_length=10, choices=POSITION_CHOICES)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
