0d71def43736dd52cb23623de8311aa23752099d
AbigailUhl <abbyuhl2022@gmail.com>
2026-04-14 16:20:40 -0400

    add PlayerForm and connect list view to detail

---
+++ project3/myapp/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Player
+
+
+class PlayerForm(forms.ModelForm):
+    class Meta:
+        model = Player
+        fields = [
+            "name",
+            "nationality",
+            "team",
+            "national_position",
+            "club_position",
+            "rating",
+            "height",
+            "weight",
+            "birth_date",
+            "age",
+            "skill_moves",
+            "ball_control",
+            "dribbling",
+            "speed",
+            "strength",
+            "player_level",
+        ]
--- project3/myapp/views.py
+++ project3/myapp/views.py
@@ -1,17 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
 from .models import Player
-
+from .forms import PlayerForm
 
 def home(request):
     return render(request, "myapp/home.html")
 
-
 def record_list(request):
     records = Player.objects.all().order_by("id")
-
     paginator = Paginator(records, 20)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
-
     return render(request, "myapp/list.html", {"page_obj": page_obj})
+
+def record_detail(request, pk):
+    record = get_object_or_404(Player, pk=pk)
+    return render(request, "myapp/detail.html", {"record": record})
+
+def record_add(request):
+    if request.method == "POST":
+        form = PlayerForm(request.POST)
+        if form.is_valid():
+            record = form.save()
+            return redirect("record_detail", pk=record.pk)
+    else:
+        form = PlayerForm()
+
+    return render(request, "myapp/form.html", {"form": form, "title": "Add Player"})
+
+def record_edit(request, pk):
+    record = get_object_or_404(Player, pk=pk)
+
+    if request.method == "POST":
+        form = PlayerForm(request.POST, instance=record)
+        if form.is_valid():
+            record = form.save()
+            return redirect("record_detail", pk=record.pk)
+    else:
+        form = PlayerForm(instance=record)
+
+    return render(request, "myapp/form.html", {"form": form, "title": "Edit Player"})
+
+def record_delete(request, pk):
+    record = get_object_or_404(Player, pk=pk)
+
+    if request.method == "POST":
+        record.delete()
+        return redirect("record_list")
+
+    return render(request, "myapp/confirm_delete.html", {"record": record})
