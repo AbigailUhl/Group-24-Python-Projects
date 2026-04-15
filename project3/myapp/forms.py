7a56914bb8cca5d701f495085fd9885c16f1fbed
AbigailUhl <abbyuhl2022@gmail.com>
2026-04-15 17:31:25 -0400

    Add boostrap

--- project3/myapp/forms.py
+++ project3/myapp/forms.py
@@ -23,3 +23,14 @@ class PlayerForm(forms.ModelForm):
             "strength",
             "player_level",
         ]
+
+class PlayerForm(forms.ModelForm):
+    class Meta:
+        model = Player
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
